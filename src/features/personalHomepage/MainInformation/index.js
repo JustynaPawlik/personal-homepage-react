5ff47871eff82e75ff39e96c06748058cae74dcc
Justyna Pawlik <juspaw11@wp.pl>
2021-10-27 19:37:29 +0200

    added email in ButtonLink

--- src/features/personalHomepage/MainInformation/index.js
+++ src/features/personalHomepage/MainInformation/index.js
@@ -1,5 +1,6 @@
 import { Avatar, ButtonIcon, Name, StyledButtonLink, Summary, ThisIs, Wrapper } from "./styled"
 import JustynaPawlikProfile from "./JustynaPawlikProfile.jpg";
+import { email } from "../email";
 
 export const MainInformation = () => (
   <Wrapper>
@@ -10,7 +11,7 @@ export const MainInformation = () => (
       <Summary>
         👩💻 I am a passionate person about programming, especially in JavaScript and React. And now I'm looking for new job opportunities.
        </Summary>
-       <StyledButtonLink>
+       <StyledButtonLink href={`mailto:${email}`} title={email}>
         <ButtonIcon />
         Hire me
        </StyledButtonLink>
---
+++ src/features/personalHomepage/email.js
@@ -0,0 +1,1 @@
+export const email = "pawlikjustyna90@wp.pl";
