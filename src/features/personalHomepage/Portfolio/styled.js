4a6d14933e07936d2491db1589602672a0d003c0
Justyna Pawlik <juspaw11@wp.pl>
2021-10-27 20:46:53 +0200

    added margin and centre Portfolio

--- src/features/personalHomepage/Portfolio/styled.js
+++ src/features/personalHomepage/Portfolio/styled.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 import { ReactComponent as GithubIcon } from "./github.svg";
 
 export const Section = styled.section`
+margin-top: 72px;
 `;
 
 export const Header = styled.header`
+text-align: center;
 `;
 
 export const MyRecentProjects = styled.h3`
