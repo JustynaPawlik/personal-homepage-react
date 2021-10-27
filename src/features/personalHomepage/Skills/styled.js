9332d4a4278d79f62f5a63eabcf8ad3a2046ead2
Justyna Pawlik <juspaw11@wp.pl>
2021-10-27 19:21:53 +0200

    added basic styled for Skills

--- src/features/personalHomepage/Skills/styled.js
+++ src/features/personalHomepage/Skills/styled.js
@@ -1,22 +1,34 @@
 import styled from "styled-components";
+import { ReactComponent as BulletIcon } from "./bullet.svg";
 
-export const Section = styled.p`
+export const Section = styled.section`
+    padding: 32px;
+    margin-top: 72px;
+    }
+`;
+
+export const StyledHeader = styled.h2`
+    padding-bottom: 16px;
+`;
+
+export const Item = styled.li`
+    display: flex;
+    line-height: 1.4;
 }
 `;
 
-export const StyledHeader = styled.p`
-}
-`;
-
-export const Item = styled.p`
-}
-`;
-
-export const List = styled.p`
-}
+export const List = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 8px 32px;
+    list-style: none;
+    margin-top: 32 px;
+    }
 `;
 
 
-export const Bullet = styled.p`
+export const Bullet = styled(BulletIcon)`
+    margin-right: 16px;
+    height: auto;
 }
 `;
