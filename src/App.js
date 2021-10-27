c806f5bb934fdd7c5e69cf03e28af723b2f43f50
Justyna Pawlik <juspaw11@wp.pl>
2021-10-27 17:36:37 +0200

    added theme and PersonalHomepage Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./GlobalStyle";
+import { PersonalHomepage } from "./features/personalHomepage";
+import { theme } from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Normalize />
     <GlobalStyle />
+    <PersonalHomepage />
     </ThemeProvider>
   );
 }
