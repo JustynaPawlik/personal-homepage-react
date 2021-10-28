ed5cd04919ec9c1d6888b59d57059533b077ac24
Justyna Pawlik <juspaw11@wp.pl>
2021-10-28 16:40:46 +0200

    added store

---
+++ src/core/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import personalHomepageReducer from "../features/personalHomepage/personalHomepageSlice";
+
+import saga from "./saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+    reducer: {
+        personalHomepage: personalHomepageReducer,
+    },
+    middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(saga);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./core/App";
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import store from './core/store';
 
 ReactDOM.render(
   <React.StrictMode>
+     <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
