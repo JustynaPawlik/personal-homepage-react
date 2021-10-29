1643adc9203491fc8e793d465790cb0d15f8024a
Justyna Pawlik <juspaw11@wp.pl>
2021-10-29 16:23:58 +0200

    added other styles in ThemeSwitch

--- src/common/ThemeSwitch/styled.js
+++ src/common/ThemeSwitch/styled.js
@@ -1,21 +1,27 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { ReactComponent as SunIcon } from "./sun.svg";
 
 export const Wrapper = styled.div`
-display: flex;
-justify-content: flex-end;
+    display: flex;
+    justify-content: flex-end;
 `;
 
 export const Button = styled.button`
-cursor: pointer;
-border:none;
-display: flex;
-align-items: center;
-color: inherit;
-outline-offset: 8px;
-background: none;
+    cursor: pointer;
+    border:none;
+    display: flex;
+    align-items: center;
+    color: inherit;
+    outline-offset: 8px;
+    background: none;
 `;
 
 export const Text = styled.span`
+    font-size: 12px;
+    text-transform: uppercase;
+    font-weight: bold;
+    margin-right: 12px;
+
 `;
 
 export const Box = styled.span`
@@ -28,8 +34,15 @@ width: 48px;
 `;
 
 export const IconWrapper = styled.span`
-
+    display:flex;
+    background: currentColor;
+    border-radius: 50%;
+    transition: transform 0.3s;
+    ${({ moveToRight }) => moveToRight && css`
+      transform: translateX(20px);
+    `}
 `;
 
-export const Icon = styled.img`
+export const Icon = styled(SunIcon)`
+color:${({theme}) => theme.colors.themeSwitch.icon};
 `;
