30946c7da77db71b79b23736d9a8f8b2746003df
Justyna Pawlik <juspaw11@wp.pl>
2021-10-30 17:24:34 +0200

    added hover, border-radius and media queries in ButtonLink

--- src/features/personalHomepage/ButtonLink/index.js
+++ src/features/personalHomepage/ButtonLink/index.js
@@ -6,8 +6,15 @@ export const ButtonLink = styled.a`
   font-weight: 600;
   text-decoration: none;
   transition: box-shadow 0.3s;
-  border: 1px solid;
+  border: 1px solid ${({ theme }) => theme.colors.buttonLink.border};
   background: ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.buttonLink.text};
+  border-radius: ${({ theme }) => theme.borderRadiusSmall};
+  
+  &:hover {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.buttonLink.shadow};
   }
-  `;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 18px;
+  }
+`;
