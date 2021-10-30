7c5d4af067a8d201e0e0820bfa18fb00b3d882e4
Justyna Pawlik <juspaw11@wp.pl>
2021-10-30 17:06:39 +0200

    removed unneeded lines from GlobalStyle

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -8,19 +8,7 @@ html {
   *, ::after, ::before {
     box-sizing: inherit;
   }
-  
-  padding-bottom: 108px;
-    font-family: "Inter", sans-serif;
-    font-size: 18px;
-    letter-spacing: 0.05em;
-    word-break: break-word;
-    overflow-y: scroll;
-    transition: background .3s;
-    
-    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-        padding-bottom: 32px;
-    }
-}
+
 
 body {
   padding-bottom: 108px;
